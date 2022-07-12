94beb716eaecc48c510d2b8331b55dbcb3f27391
Гуськов Владимир <raz3r444uk@mail.ru>
2022-07-12 07:33:20 +0400

    init: first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const Counter = (props) => {
+  const { value } = props;
+
+  const formatValue = () => {
+    return value === 0 ? "empty" : value;
+  };
+
+  const getBadgesClasses = () => {
+    let classes = "badge m-2 ";
+    classes += value === 0 ? "bg-warning" : "bg-primary";
+    return classes;
+  };
+
+  const handleIncrement = () => {
+    console.log(props);
+  };
+
+  const handleDecrement = () => {
+    console.log(props);
+  };
+
+  return (
+    <div>
+      <span>{props.name}</span>
+      <span className={getBadgesClasses()}>{formatValue()}</span>
+      <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
+        +
+      </button>
+      <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
+        -
+      </button>
+
+      <button className="btn btn-danger btn-sm m-2" onClick={() => props.onDelete(props.id)}>
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ src/components/countersList.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Counter from "./counter";
+
+const CountersList = () => {
+  const initalState = [
+    { id: 0, value: 0, name: "Ненужная вещь", price: 200 },
+    { id: 1, value: 4, name: "Ложка" },
+    { id: 2, value: 0, name: "Вилка" },
+    { id: 3, value: 0, name: "Тарелка" },
+    { id: 4, value: 0, name: "Набор минималиста" },
+  ];
+
+  const [counters, setCounters] = useState(initalState);
+
+  const handleDelete = (id) => {
+    const newCounters = counters.filter((counter) => counter.id != id);
+    setCounters(newCounters);
+  };
+
+  const hanldeReset = () => {
+    setCounters(initalState);
+  };
+
+  const hanldePlus = () => {
+    setCounters(initalState.value + 1);
+  };
+
+  const hanldeMinus = () => {
+    setCounters(initalState.value - 1);
+  };
+
+  return (
+    <>
+      {counters.map((count) => (
+        <Counter
+          key={count.id}
+          onDelete={handleDelete}
+          onIncrement={hanldePlus}
+          onDecrement={hanldeMinus}
+          {...count}
+        />
+      ))}
+      <button className="btn btn-primary btb-sm m-2" onClick={hanldeReset}>
+        Reset
+      </button>{" "}
+    </>
+  );
+};
+
+export default CountersList;
